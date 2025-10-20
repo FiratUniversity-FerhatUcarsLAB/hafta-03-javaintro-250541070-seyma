8d3de0a5f76fea3da49c0ae4d1903afa2c8c43cd
250541070-seyma <250541070@firat.edu.tr>
2025-10-20 15:07:35 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,10 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class AsciiArt {
+    public static void main(String[] args) {
+        System.out.println("S   S");
+        System.out.println("SS  S");
+        System.out.println("S S S");
+    }
+}
