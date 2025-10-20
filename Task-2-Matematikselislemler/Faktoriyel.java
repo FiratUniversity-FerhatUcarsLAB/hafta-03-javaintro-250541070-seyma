dca93ea7ae936d0ae5089e1b418bd0c67d0ec402
250541070-seyma <250541070@firat.edu.tr>
2025-10-20 14:03:58 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class Faktoriyel {
+    public static void main(String[] args) {
+        // Sonucu hesapla
+        int sonuc = 1*2*3*4*5;
+        
+        // Ekrana yazdır
+        System.out.println("1*2*3*4*5 = " + sonuc);
+    }
+}
