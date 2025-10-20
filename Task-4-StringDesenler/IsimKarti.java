d89b15f9967e117d7b2219e6f8939ede1a358f9a
250541070-seyma <250541070@firat.edu.tr>
2025-10-20 14:59:16 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Seyma";
+        String soyad = "Katar";
+        String ogrNo = "250541070";
+        String bolum = "Yazilim Muh";
+        
+        System.out.println("===================");
+        System.out.println(" OGRENCİ BİLGİLERİ ");
+        System.out.println("===================");
+        System.out.println("Ad    : " + ad);
+        System.out.println("Soyad : " + soyad);
+        System.out.println("No    : " + ogrNo);
+        System.out.println("Bolum : " + bolum);
+        System.out.println("===================");
+    }
+}
