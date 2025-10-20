3085cedd3a1cc799185a486554e90f581f484d89
250541070-seyma <250541070@firat.edu.tr>
2025-10-20 13:46:17 +0300

    Update BesKereTekrar.java

--- Task-1-HosgeldinMesajlari/BesKereTekrar.java
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -1,7 +1,7 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 
-class Main {
+public class JavayaHoşGeldiniz {
     public static void main(String[] args) {
         System.out.println("Try programiz.pro");
         System.out.println("Java'ya Hoş Geldiniz");
