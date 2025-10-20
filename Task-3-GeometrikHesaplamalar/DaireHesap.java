76ffd43932a67f930b13382733a97de2f09ac612
250541070-seyma <250541070@firat.edu.tr>
2025-10-20 14:21:41 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,19 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class DaireHesap {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+        
+        // Alan hesapla
+        double alan = en*boy;
+        
+        // Cevre hesapla
+        double cevre = (en+boy)*2;
+        
+        // Sonucları hesapla
+        System.out.println("Alan: " + alan);
+        System.out.println("Cevre: " + cevre);
+    }
+}
