617cbeeab69fb7a2a09dd9db92d73a437c947497
250541070-seyma <250541070@firat.edu.tr>
2025-10-20 15:14:05 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,9 @@
+public class HesapOzeti {
+    public static void main(String[] args) {
+        System.out.println("Urün\tMiktar\tFiyat");
+        System.out.println("-------------------");
+        System.out.println("Makarna\t2\t15");
+        System.out.println("elma\t5\t40");
+        System.out.println("yogurt\t2\t50");
+    }
+}
