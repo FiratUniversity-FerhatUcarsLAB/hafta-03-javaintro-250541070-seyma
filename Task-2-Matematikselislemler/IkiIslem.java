a4a368bff7c3e97b079d2a0385f93ed834b89d9a
250541070-seyma <250541070@firat.edu.tr>
2025-10-20 14:13:31 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,17 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class IkiIslem {
+    public static void main(String[] args) {
+        // Pay hesapla
+        double pay = (9.5*4.5) - (2.5*3);
+        
+        // Payda hesapla
+        double payda = 45*5 - 3.5;
+        
+        // Bolme yap
+        double sonuc = pay / payda;
+        // Formatla ve yazdir
+        System.out.printf("%.4f\n", sonuc);
+    }
+}
